83c12908458e95d3ab38d94d9fa1fb967125ea6b
RyanIaro <hei.ryan.3@gmail.com>
2023-03-21 10:34:34 +0300

    feat: implemented endpoint GET /students/{student_id}/courses

--- src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.hei.haapi.endpoint.rest.mapper.CourseMapper;
 import school.hei.haapi.endpoint.rest.model.Course;
+import school.hei.haapi.model.User;
 import school.hei.haapi.service.CourseService;
+import school.hei.haapi.service.UserService;
 
 import java.util.List;
 
@@ -19,6 +22,7 @@ import static java.util.stream.Collectors.toUnmodifiableList;
 public class CourseController {
 
     private final CourseService service;
+    private final UserService userService;
     private final CourseMapper mapper;
 
     @GetMapping("/courses/{id}")
@@ -41,6 +45,17 @@ public class CourseController {
                     .stream()
                     .map(mapper::toDomain)
                     .collect(toUnmodifiableList()))
+                .stream()
+                .map(mapper::toRest)
+                .collect(toUnmodifiableList());
+    }
+
+    @GetMapping("/students/{student_id}/courses")
+    public List<Course> getStudentsCourses(
+            @PathVariable String student_id,
+            @RequestParam(value = "status", required = false, defaultValue = "LINKED") String status) {
+        User student = userService.getById(student_id);
+        return student.getCourseStatus()
                 .stream()
                 .map(mapper::toRest)
                 .collect(toUnmodifiableList());
